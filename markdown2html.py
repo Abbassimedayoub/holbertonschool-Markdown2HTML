4ff65a5e2409b638586a07283894f2e530a008c2
abbassimedayoub <abbassimedayoub@gamil.com>
2024-08-07 21:36:07 +0200

    script that take an arguements 2 strings

--- markdown2html.py
+++ markdown2html.py
@@ -8,12 +8,15 @@ def main():
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
+
     markdown_file = sys.argv[1]
     html_file = sys.argv[2]
+
     # Check if the Markdown file exists
     if not os.path.isfile(markdown_file):
         print(f"Missing {markdown_file}", file=sys.stderr)
         sys.exit(1)
+
     # If all checks pass, exit with status code 0
     sys.exit(0)
 
