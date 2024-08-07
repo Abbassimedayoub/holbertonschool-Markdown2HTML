1e3b545bbc9041340319c74a3881a02d1893546b
abbassimedayoub <abbassimedayoub@gamil.com>
2024-08-07 21:38:32 +0200

    change from print to wirte

--- markdown2html.py
+++ markdown2html.py
@@ -6,7 +6,7 @@ import os
 def main():
     # Check if the number of arguments is less than 2
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
 
     markdown_file = sys.argv[1]
@@ -14,7 +14,7 @@ def main():
 
     # Check if the Markdown file exists
     if not os.path.isfile(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.stderr.write(f"missing {markdown_file}\n")
         sys.exit(1)
 
     # If all checks pass, exit with status code 0
