3764794ef83c02f1211c820bf26ad948189ee079
abbassimedayoub <abbassimedayoub@gamil.com>
2024-08-07 21:41:11 +0200

    chaneg

--- markdown2html.py
+++ markdown2html.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 import sys
 import os
 
@@ -6,7 +7,7 @@ import os
 def main():
     # Check if the number of arguments is less than 2
     if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
     markdown_file = sys.argv[1]
@@ -14,10 +15,10 @@ def main():
 
     # Check if the Markdown file exists
     if not os.path.isfile(markdown_file):
-        sys.stderr.write(f"missing {markdown_file}\n")
+        print(f"Missing {markdown_file}", file=sys.stderr)
         sys.exit(1)
 
-    # If all checks pass, exit with status code 0
+    # If all checks pass, print nothing and exit with status code 0
     sys.exit(0)
 
 
